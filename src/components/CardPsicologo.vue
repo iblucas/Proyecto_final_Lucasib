e4fbf48884bf0d2205be8a74621c12ec87a8503c
iblucas <lucasibar2021@gmail.com>
2023-07-18 20:16:49 +0200

    card quiero escucharte

--- src/components/CardPsicologo.vue
+++ src/components/CardPsicologo.vue
@@ -1,4 +1,17 @@
 <template>
+
+<div class="card bg-dark text-white">
+  <img src="../assets/img/escucharte.png" class="card-img" alt="psicologia">
+  <div class="card-img-overlay">
+    
+  </div>
+</div>
+
+<br>
+
+
+
+
     <div class="row row-cols-1 row-cols-md-2 g-4">
   <div class="col">
     <div class="card">
@@ -18,9 +31,12 @@
       </div>
     </div>
   </div>
+
+<br>
+
   <div class="col">
     <div class="card">
-      <img class="../assets/img/estres.jpg" alt="estres mallorca">
+      <img src="../assets/img/estres.jpg" alt="estres mallorca">
       <div class="card-body">
         <h5 class="card-title">Tratamiento del Estrés</h5>
         <p class="card-text">El estrés es una reacción natural de nuestro organismo cuya finalidad es la adaptación a las demandas del entorno. Para poder enfrentarnos con éxito a las demandas del día a día nuestro cuerpo moviliza una serie de recursos, tanto a nivel mental, como físico y conductual</p>
@@ -33,12 +49,12 @@
       <div class="card-body">
         <h5 class="card-title">Psicologo experto en Adicciones</h5>
         <p class="card-text">La psicología de las adicciones es la rama de la psicología clínica que busca ayudar a las personas que padecen una adicción y que quieren aprender a vivir sin ella. Un psicólogo especialista en adicciones cuenta con una formación adecuada para tratar las adicciones y tenemos herramientas científicamente validadas y profundamente estudiadas mediante la experiencia y los conocimientos universitarios en el campo de las adicciones.</p>
-        /div>
       </div>
     </div>
   </div>
 </div>
 
+<br>
 
 </template>
 
